32126194c99044cbd81c53984739b743efcccb90
SzJ2631 <2631454154@qq.com>
2020-02-12 12:53:59 +0800

    A

---
+++ hello.c
@@ -0,0 +1,9 @@
+//
+//  hello.c
+//  WorkSpace
+//
+//  Created by Nakano Azusa on 2020/2/12.
+//  Copyright © 2020 Nakano Azusa. All rights reserved.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,14 @@
+//
+//  hello.h
+//  WorkSpace
+//
+//  Created by Nakano Azusa on 2020/2/12.
+//  Copyright © 2020 Nakano Azusa. All rights reserved.
+//
+
+#ifndef hello_h
+#define hello_h
+
+#include <stdio.h>
+
+#endif /* hello_h */
